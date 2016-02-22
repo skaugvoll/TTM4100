a8c95eb6380c5e1cad73513e79b07b5640593738
Martin Stigen <Martin Stigen>
2016-02-22 13:34:55 +0100

    La til litt kode

--- TTM4100/src/application/Client.java
+++ TTM4100/src/application/Client.java
@@ -1,5 +1,28 @@
 package application;
 
-public class Client {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
 
+public class Client {
+	Socket socket;
+	BufferedReader in;
+	PrintWriter out;
+	
+	public Client(String address) throws Exception{
+		socket = new Socket(InetAddress.getByName(address), 4321);
+		out = new PrintWriter(socket.getOutputStream(), true);
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	}
+	
+	public void send(String msg) {
+		try {
+			out.println(msg);
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- TTM4100/src/application/Server.java
+++ TTM4100/src/application/Server.java
@@ -1,5 +1,63 @@
 package application;
 
-public class Server {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+import javafx.application.Platform;
+
+public class Server implements Runnable{
+	private Socket socket;
+	private ServerSocket server;
+	private final Main main;
+	
+	public Server(Main main) {
+		this.main = main;
+		
+		try {
+			server = new ServerSocket(4321, 10, InetAddress.getLocalHost());
+		}
+		catch (IOException e) {
+			System.out.println("Noe gikk galt");
+			e.printStackTrace();
+		}
+	}
+	
+	public String getAddress() {
+		return server.getInetAddress().toString().split("/")[1];
+	}
+
+	@Override
+	public void run() {
+		try {
+			socket = server.accept();
+			String clientIP = socket.getRemoteSocketAddress().toString().substring(1).split(":")[0];
+			Platform.runLater(new Runnable() {
+				
+				@Override
+				public void run() {
+//					main.connectedToYou(clientIP);
+				}
+			});
+			while (true){
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+				String line = in.readLine();
+				Platform.runLater(new Runnable() {
+					
+					@Override
+					public void run() {
+//						main.recieve(line);
+					}
+				});
+			}
+		}
+		catch (IOException e) {
+			System.out.println("Read failed");
+			System.exit(-1);
+		}
+	}
 }
