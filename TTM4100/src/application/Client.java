fd1f9cd1926b1eade84def584ff98bbe7b0c5765
Martin Stigen <Martin Stigen>
2016-02-22 14:02:39 +0100

    Litt endringer i Client-klassen

--- TTM4100/src/application/Client.java
+++ TTM4100/src/application/Client.java
@@ -1,28 +1,30 @@
 package application;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 
 public class Client {
-	Socket socket;
-	BufferedReader in;
-	PrintWriter out;
 	
-	public Client(String address) throws Exception{
-		socket = new Socket(InetAddress.getByName(address), 4321);
-		out = new PrintWriter(socket.getOutputStream(), true);
-		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	private Socket socket;
+	private BufferedReader in;
+	private PrintWriter out;
+	
+	public Client(String address){
+		try {			
+			socket = new Socket(InetAddress.getByName(address), 4321);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new PrintWriter(socket.getOutputStream(), true);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void send(String msg) {
-		try {
-			out.println(msg);
-		} 
-		catch (Exception e) {
-			e.printStackTrace();
-		}
+		out.println(msg);
 	}
 }
